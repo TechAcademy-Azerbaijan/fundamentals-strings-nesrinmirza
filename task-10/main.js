4f614513aab2bee4dc3526fac16c4224d02a60ba
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-07-08 09:21:20 -0700

    Update main.js

--- task-10/main.js
+++ task-10/main.js
@@ -1,9 +1,21 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let arr = str.split("")
+let index = [];
+let first = 0;
+let last = 0;
+let isFound = false;
+for (let i = 0; i < arr.length; i++) {
+   
+  if (arr[i] == " " ) {
+   index.push([i])
+   first = index[0];
+   last = index[index.length-1]
+   isFound = true;
+  } 
+}
+ if (isFound==true){
+console.log(first);
+console.log(last);
+ }
+ else {
+    console.log("-1");
+ }
