fb6bb0741888819c9f743f280d93069ed616b831
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-07-08 09:54:48 -0700

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,23 @@
-const prompt = require('prompt');
-prompt.start();
+let str = "55555222"
+let arr = str.split("")
 
+let counta = 0;
+let countb=0;
+for (let i = 0; i < arr.length; i++) {
+if (arr[i]==5) {
+    counta++;
+}
+else if (arr[i]==2) {
+    countb++;
+}
+}
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+if (counta>countb) {
+    console.log("5");
+}
+else if (counta<countb) {
+    console.log("2");
+}
+else if (counta==countb) {
+    console.log("=");
+}
