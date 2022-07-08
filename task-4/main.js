b45c352839b41423b8fbad4ef0c5bced7ce51a21
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-07-08 10:05:41 -0700

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,14 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let str = "Hello world! I learn to code."
+let arr = str.split("")
+let a = 0;
+let b = 0;
+for (let i = 0; i < arr.length; i++) {
+   if (arr[i]=="!") {
+   a++;
+   }
+   else if (arr[i]==".") {
+    b++;
+   }
+    
+}
+console.log(a+b);
