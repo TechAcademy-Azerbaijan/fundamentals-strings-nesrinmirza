b91ff7795e349921d16e4db12206e35eea508c6f
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-07-12 00:13:28 -0700

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,9 +1,9 @@
-const prompt = require('prompt');
-prompt.start();
+let str = "Hello world! Hello,    country!"
+let count =1;
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+for (let i = 0; i < str.length; i++) {
+    if (str[i]===" " && str[i+1]!=" ") {
+        count++; 
+    }
+}
+console.log(count);
